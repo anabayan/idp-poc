55d732f733eedc1a44b699faa494faad8e8efe25
anabayan <anabayan.kris@gmail.com>
2020-06-08 16:30:02 -0500

    Create structures

---
+++ src/portalui/src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SiteComponent } from './site/site.component';
+
+
+
+@NgModule({
+  declarations: [SiteComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
