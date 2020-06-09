a04da18c720cac935d64026c6ea5cc91122ee8f2
anabayan <anabayan.kris@gmail.com>
2020-06-09 11:01:04 -0500

    folder structure changes

--- src/portalui/src/app/layout/layout.module.ts
+++ src/portalui/src/app/layout/layout.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [NavbarComponent, FooterComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
+  ],
+  exports:[
+    NavbarComponent
   ]
 })
 export class LayoutModule { }
--- src/portalui/src/app/shared/shared.module.ts
+++ src/portalui/src/app/shared/shared.module.ts
@@ -1,13 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SiteComponent } from './site/site.component';
+import { SpinnerComponent } from './spinner/spinner.component';
+import { MultiselectComponent } from './multiselect/multiselect.component';
 
 
 
 @NgModule({
-  declarations: [SiteComponent],
+  declarations: [SiteComponent, SpinnerComponent, MultiselectComponent],
   imports: [
     CommonModule
+  ],
+  exports:[
+    SiteComponent
   ]
 })
 export class SharedModule { }
